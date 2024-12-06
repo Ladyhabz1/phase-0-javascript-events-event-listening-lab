e3ad1eed42dd4bc7b25f617a83c9c113323ac7bd
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2024-12-07 01:13:20 +0300

    Added the addingEventListener() function

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button'); // Get the button element
+    button.addEventListener('click', function () { // Add a 'click' event listener
+      alert('Button was clicked!'); // Show an alert when the button is clicked
+    });
+  }
+  
